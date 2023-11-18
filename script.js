d58718680437fa6d1fd6d8830b30e9f1d540f85d
Vivek Rathore <147583851+Rathore-Vivek@users.noreply.github.com>
2023-11-18 21:04:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('#main', function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          const offsetTop = targetElement.offsetTop;
+          scroll({
+            top: offsetTop,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
